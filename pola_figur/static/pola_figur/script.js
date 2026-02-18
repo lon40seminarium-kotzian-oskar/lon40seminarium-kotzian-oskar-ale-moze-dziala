9fff335dc6b57631038d9ab61fc417276b0c2508
lon40seminarium-kotzian-oskar <benekjebanyprzeztwojamatke@gmail.com>
2026-02-18 13:13:55 +0100

    Dodany trójkąt

--- pola_figur/static/pola_figur/script.js
+++ pola_figur/static/pola_figur/script.js
@@ -18,3 +18,24 @@ function circle(){
     document.getElementById("cirResult").innerHTML =
         `Pole: ${area.toFixed(3)}<br>Obwód: ${perimeter.toFixed(3)}`;
 }
+
+function triangleArea(){
+    let h = Number(document.getElementById('trH').value);
+    let a = Number(document.getElementById('trA').value);
+
+    let area = (a * h) / 2;
+
+    document.getElementById("trAResult").innerHTML =
+    `Pole: ${area}`;
+}
+
+function triangleDiameter(){
+    let a = Number(document.getElementById('trDA').value);
+    let b = Number(document.getElementById('trB').value);
+    let c = Number(document.getElementById('trC').value);
+    
+    let perimeter = a+b+c;
+
+    document.getElementById("trDResult").innerHTML = 
+    `obwód: ${perimeter}`;
+}
