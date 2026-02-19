65ba5f1babb8423c5a986d2a2baeb09f7b38fb5f
lon40seminarium-kotzian-oskar <benekjebanyprzeztwojamatke@gmail.com>
2026-02-19 16:08:26 +0100

    Dodałem operacjie na ułamkach dodawanie

--- operacje_na_ulamkach/static/operacje_na_ulamkach/script.js
+++ operacje_na_ulamkach/static/operacje_na_ulamkach/script.js
@@ -31,8 +31,32 @@ function dodajUlamek(){
     let l2 = Number(document.getElementById('l3').value);
     let m2 = Number(document.getElementById('m3').value);
 
-    if(m1===m2){
-        document.getElementById('lw').innerHTML = l1+l2;
-        document.getElementById('lw').innerHTML = m1;
+    if (m1 === 0 || m2 === 0) {
+        alert("Mianownik nie może być zerem");
+        return;
     }
+
+    let licznik = l1 * m2 + l2 * m1;
+    let mianownik = m1 * m2;
+
+    document.getElementById('lw').value = licznik;
+    document.getElementById('mw').value = mianownik;
+}
+
+function odejmijUlamek(){
+    let l1 = Number(document.getElementById('l2').value);
+    let m1 = Number(document.getElementById('m2').value);
+    let l2 = Number(document.getElementById('l3').value);
+    let m2 = Number(document.getElementById('m3').value);
+
+    if (m1 === 0 || m2 === 0) {
+        alert("Mianownik nie może być zerem");
+        return;
+    }
+
+    let licznik = l1 * m2 - l2 * m1;
+    let mianownik = m1 * m2;
+
+    document.getElementById('lw').value = licznik;
+    document.getElementById('mw').value = mianownik;
 }
