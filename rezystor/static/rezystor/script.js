b81fef364f54cf667f648f8c509fe793a8696e21
OskiBoski16 <okotzian@gmail.com>
2026-03-05 15:46:23 +0100

    no dodane rezystor v2

---
+++ rezystor/static/rezystor/script.js
@@ -0,0 +1,15 @@
+function showMenu1(){
+    document.getElementById('value').style.display = 'block';
+}
+
+function readValue1(){
+    var a = document.getElementById('value1').value;
+    var bg = document.getElementById('belt1').style.background-color;
+    if(a == 0){
+        bg = 'black';
+    } else if (a == 1){
+        bg = 'brown'
+    } else if (a == 2){
+        bg = 'red'
+    }
+}
