8947042be332f4da6561417f37c64a57ed840e85
OskiBoski16 <okotzian@gmail.com>
2026-03-05 16:01:44 +0100

    no dodane rezystor v5

--- rezystor/static/rezystor/script.js
+++ rezystor/static/rezystor/script.js
@@ -2,14 +2,15 @@ function showMenu1(){
     document.getElementById('value1').style.display = 'block';
 }
 
-function readValue1(){
+function readValue1() {
     var a = document.getElementById('value1').value;
-    var bg = document.getElementById('belt1').style['background-color'];
-    if(a == 0){
-        bg = 'black';
-    } else if (a == 1){
-        bg = 'brown';
-    } else if (a == 2){
-        bg = 'red';
+    var belt = document.getElementById('belt1');
+
+    if (a == 0) {
+        belt.style.backgroundColor = 'black';
+    } else if (a == 1) {
+        belt.style.backgroundColor = 'brown';
+    } else if (a == 2) {
+        belt.style.backgroundColor = 'red';
     }
 }
