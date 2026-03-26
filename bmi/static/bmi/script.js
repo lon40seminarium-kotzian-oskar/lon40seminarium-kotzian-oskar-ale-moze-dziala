29ac6b473006b7c9226746a577f53fd5310b4a47
OskiBoski16 <okotzian@gmail.com>
2026-03-26 15:13:04 +0100

    dodane bmi v6

--- bmi/static/bmi/script.js
+++ bmi/static/bmi/script.js
@@ -1,3 +1,11 @@
+const bmiScale = {
+    under: { degmax : -85 , degmin : -120, max :21 },
+    normal: { degmax : -34  , degmin : -84 },
+    over: { degmax : 33 , degmin : -33 },
+    obese: { degmax : 84 , degmin : 34 },
+    extreamly: { degmax : 120, degmin : 85 }
+};
+
 function oblicz(){
     const waga = parseFloat(document.getElementById('waga').value);
     const wzrost = parseFloat(document.getElementById('wzrost').value)/100;
